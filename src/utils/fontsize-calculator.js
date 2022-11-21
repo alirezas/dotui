42bae94a2c0e16c9baf5967fb00349206f010e8e
Alireza Sarabchi <a.sarabchi@gmail.com>
2022-11-21 17:33:35 +0330

    fontSize now calculation based on Tailwind's default configs.

--- src/utils/fontsize-calculator.js
+++ src/utils/fontsize-calculator.js
@@ -1,21 +1,11 @@
-module.exports = ({ baseFontSize, typeScale, fontSizeUnit }) => {
-  const DEFAULT_SCALES = {
-    '7xl': 8,
-    '6xl': 7,
-    '5xl': 6,
-    '4xl': 5,
-    '3xl': 4,
-    '2xl': 3,
-    xl: 2,
-    lg: 1,
-    base: 0,
-    tiny: -1,
-    sm: -2,
-    xs: -3
-  }
+const defaultConfig = require('tailwindcss/defaultConfig')
 
-  return Object.keys(DEFAULT_SCALES).reduce((acc, size) => {
-    const exponent = DEFAULT_SCALES[size]
+module.exports = ({ baseFontSize, typeScale, fontSizeUnit }) => {
+  const defaultFontSizes = defaultConfig.theme.fontSize
+  const indexOfBase = Object.keys(defaultFontSizes).findIndex((el) => el === 'base')
+
+  return Object.keys(defaultFontSizes).reduce((acc, size, index) => {
+    const exponent = index - indexOfBase
     const multiplier = parseFloat(
       (fontSizeUnit === 'px' ? baseFontSize : 1) * Math.pow(typeScale, exponent)
     ).toPrecision(4)
